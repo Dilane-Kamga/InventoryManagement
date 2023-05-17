f2d0f10fd878fffb0611f1593ee0855ee75e693c
Dilane-Kamga <dilanekamga777@hotmail.com>
2023-05-17 12:46:00 +0100

    add interceptor

--- src/main/java/com/kamtech/inventorymanagement/InventoryManagementApplication.java
+++ src/main/java/com/kamtech/inventorymanagement/InventoryManagementApplication.java
@@ -2,8 +2,10 @@ package com.kamtech.inventorymanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // This annotation enables JPA Auditing
 public class InventoryManagementApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/kamtech/inventorymanagement/interceptor/Interceptor.java
@@ -0,0 +1,22 @@
+package com.kamtech.inventorymanagement.interceptor;
+
+import org.hibernate.EmptyInterceptor;
+import org.springframework.util.StringUtils;
+
+public class Interceptor extends EmptyInterceptor {
+
+
+        @Override
+        public String onPrepareStatement(String sql) {
+            if (StringUtils.hasLength(sql) && sql.toLowerCase().startsWith("select")) {
+                if (sql.contains("where")){
+                    sql = sql + " and idEnterprise = 1";
+                } else {
+                    sql = sql + " where idEnterprise = 1";
+                }
+            }
+
+            return super.onPrepareStatement(sql);
+        }
+
+}
